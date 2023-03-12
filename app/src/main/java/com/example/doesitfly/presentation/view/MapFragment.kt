410b32fb5cd83414e827d84863031544ac708e4f
Manonrtnt <70939460+Manonrtnt@users.noreply.github.com>
2023-03-12 14:54:19 +0100

    [Lottie animation] implementation | json | fragment

--- app/src/main/java/com/example/doesitfly/presentation/view/MapFragment.kt
+++ app/src/main/java/com/example/doesitfly/presentation/view/MapFragment.kt
@@ -5,16 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 import com.example.doesitfly.R
+import com.example.doesitfly.presentation.viewModel.ListFlyingSiteViewModel
 
 class MapFragment : Fragment() {
+    private val viewModel by lazy { ViewModelProvider(this)[ListFlyingSiteViewModel::class.java] }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+
+        val animationView = view.findViewById<LottieAnimationView>(R.id.animation_wip)
+        animationView.playAnimation()
+        animationView.repeatCount = LottieDrawable.INFINITE
+
+        return view
     }
 
 }
