2b630fdabc00a1fd8fbe9b52a06187550a6b36a7
Manonrtnt <70939460+Manonrtnt@users.noreply.github.com>
2023-03-12 14:06:39 +0100

    [Clean code] RemoteRepository and ApiService

--- app/src/main/java/com/example/doesitfly/data/remote/ApiService.kt
+++ app/src/main/java/com/example/doesitfly/data/remote/ApiService.kt
@@ -1,38 +1,48 @@
 package com.example.doesitfly.data.remote
 
-import android.util.Log
 import kotlinx.coroutines.DelicateCoroutinesApi
 import okhttp3.*
 import java.io.IOException
 import java.util.concurrent.CompletableFuture
 
+/** API service class for network methods */
 @OptIn(DelicateCoroutinesApi::class)
 class ApiService {
 
-    // API SERVICE CLASS FOR NETWORK METHODS
+    /**
+     * This method fetches data from an API endpoint and returns it as a string
+     * @return a string representation of the data fetched from the API endpoint
+     */
     fun fetchData(): String {
+        // create an OkHttpClient instance to make HTTP requests
         val client = OkHttpClient()
 
+        // create a request to the API endpoint
         val request = Request.Builder()
             .url("https://data.ffvl.fr/json/sites.json")
             .build()
 
+        // create a CompletableFuture that will hold the response from the API
         val completableFuture = CompletableFuture<String>()
 
+        // make an asynchronous HTTP request using OkHttp
         client.newCall(request).enqueue(object : Callback {
+            // handle any errors that occur during the request
             override fun onFailure(call: Call, e: IOException) {
                 completableFuture.completeExceptionally(e)
             }
 
+            // handle the response from the API
             override fun onResponse(call: Call, response: Response) {
+                // read the response body as a string
                 val jsonResponse = response.body?.string().toString()
 
-                Log.d("TODO 22222", jsonResponse)
-
+                // complete the CompletableFuture with the response body
                 completableFuture.complete(jsonResponse)
             }
         })
 
+        // wait for the CompletableFuture to complete and return its result
         return completableFuture.get()
     }
 }
--- app/src/main/java/com/example/doesitfly/data/repository/RemoteRepository.kt
+++ app/src/main/java/com/example/doesitfly/data/repository/RemoteRepository.kt
@@ -1,27 +1,26 @@
 package com.example.doesitfly.data.repository
 
-import android.util.Log
 import com.example.doesitfly.domain.FlyingSiteBean
 import com.example.doesitfly.data.remote.ApiService
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
+// BUSINESS LOGIC (BL) TO PERFORM API METHODS
+// Note: BL is used to manages communication between an end user application and a database
 class RemoteRepository() {
-
     companion object {
-        // Init API Service class
+        /** Init [ApiService] */
         private val apiService = ApiService()
 
-        //TODO: BUSINESS LOGIC (BL) TO PERFORM OUR API METHODS
-        // Note: BL is used to manages communication between an end user application and a database
+        /**
+         * Fetches flying sites data from API and returns it as a list of FlyingSiteBean objects
+         * @return List<[FlyingSiteBean]>
+         */
         fun getFlyingSitesFromApi(): List<FlyingSiteBean> {
             val jsonData = apiService.fetchData()
-            Log.d("JSON", jsonData)
             // Convert JSON Response to List of FlyingSites
             val flyingSites = Gson().fromJson<List<FlyingSiteBean>>(jsonData, object : TypeToken<List<FlyingSiteBean>>() {}.type)
-            Log.d("TODO 0000", jsonData.toString())
             return flyingSites ?: emptyList()
         }
     }
-
 }
